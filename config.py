fa70555b0b0996057ede480ceb91cf51cdeabf51
dendenpixel <dennisquek027@gmail.com>
2026-05-12 20:20:12 +0800

    Enhance config.py with IP and MAC address settings
    Added IP and MAC address configurations for hosts and routers.

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 network1 = "10.0.1.0/24"
 network2 = "10.0.2.0/24"
 
-host_a = "10.0.1.10"
-host_b = "10.0.2.20"
+host_a_ip = "10.0.1.10"
+host_b_ip = "10.0.2.20"
 
-router_r1 = "10.0.1.1"
-router_r2 = "10.0.2.1"
+r1_iface1_ip = "10.0.1.1"   # Router R1, subnet 1 side
+r1_iface2_ip = "10.0.2.1"   # Router R1, subnet 2 side
 
+host_a_mac = "AA:AA:AA:AA:AA:AA"
+host_b_mac = "DD:DD:DD:DD:DD:DD"
+
+r1_iface1_mac = "BB:BB:BB:BB:BB:BB"
+r1_iface2_mac = "CC:CC:CC:CC:CC:CC"
+
+TTL_DEFAULT = 100
+MAX_SEGMENT_SIZE = 500  # bytes, the max 500 req
